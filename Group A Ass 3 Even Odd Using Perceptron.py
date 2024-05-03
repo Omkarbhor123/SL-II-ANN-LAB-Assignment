bf223e44c96408f34c3cbb29c875e6ee1dc85c0a
Omkar Bhor <omkarbhor360@gmail.com>
2024-05-03 23:26:24 +0530

    Update Group A Ass 3 Even Odd Using Perceptron.py

--- Group A Ass 3 Even Odd Using Perceptron.py
+++ Group A Ass 3 Even Odd Using Perceptron.py
@@ -1,47 +1,38 @@
-import numpy as np
+# Define step function
+def step_function(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
 
-# Define the Perceptron class
-class Perceptron:
-    def __init__(self, input_size, learning_rate=0.1):
-        self.weights = np.random.rand(input_size)
-        self.bias = np.random.rand()
-        self.learning_rate = learning_rate
+# Define training data
+training_data = [
+    {'input': [1, 1, 0, 0, 0, 0], 'label': 1},  # 0 (odd)
+    {'input': [1, 1, 0, 0, 0, 1], 'label': 0},  # 1 (even)
+    {'input': [1, 1, 0, 0, 1, 0], 'label': 1},  # 2 (odd)
+    {'input': [1, 1, 0, 1, 1, 1], 'label': 0},  # 3 (even)
+    {'input': [1, 1, 0, 1, 0, 0], 'label': 1},  # 4 (odd)
+    {'input': [1, 1, 0, 1, 0, 1], 'label': 0},  # 5 (even)
+    {'input': [1, 1, 0, 1, 1, 0], 'label': 1},  # 6 (odd)
+    {'input': [1, 1, 0, 1, 1, 1], 'label': 0},  # 7 (even)
+    {'input': [1, 1, 1, 0, 0, 0], 'label': 1},  # 8 (odd)
+    {'input': [1, 1, 1, 0, 0, 1], 'label': 0},  # 9 (even)
+]
 
-    def activation_function(self, x):
-        return 1 if x >= 0 else 0
+# Initialize weights
+weights = [0, 0, 0, 0, 0, 1]
 
-    def predict(self, inputs):
-        weighted_sum = np.dot(inputs, self.weights) + self.bias
-        return self.activation_function(weighted_sum)
+# Training the perceptron
+for data in training_data:
+    input_vector = data['input']
+    label = data['label']
+    output = step_function(sum(input_vector[i] * weights[i] for i in range(len(input_vector))))
+    error = label - output
+    weights = [weights[i] + input_vector[i] * error for i in range(len(weights))]
 
-    def train(self, input_vectors, labels):
-        for input_vec, label in zip(input_vectors, labels):
-            prediction = self.predict(input_vec)
-            error = label - prediction
-            self.weights += self.learning_rate * error * input_vec
-            self.bias += self.learning_rate * error
+# Taking input from the user and predicting odd or even
+j = int(input("Enter a Number (0-9): "))
+input_vector = [int(x) for x in list('{0:06b}'.format(j))]
+output = "odd" if step_function(sum(input_vector[i] * weights[i] for i in range(len(input_vector)))) == 0 else "even"
+print(j, " is ", output)
 
-# Convert ASCII digits to binary vectors
-def ascii_to_binary(char):
-    binary = np.array([int(bit) for bit in format(ord(char), '08b')])
-    return binary
-
-# Training data
-input_vectors = [ascii_to_binary(str(i)) for i in range(10)]
-labels = [0 if i % 2 == 0 else 1 for i in range(10)]
-
-# Create the Perceptron
-perceptron = Perceptron(input_size=8, learning_rate=0.1)
-
-# Train the Perceptron
-epochs = 10
-for epoch in range(epochs):
-    perceptron.train(input_vectors, labels)
-
-# Test the Perceptron
-print("ASCII Digit | Even/Odd")
-print("------------|---------")
-for digit in range(10):
-    input_vec = ascii_to_binary(str(digit))
-    prediction = perceptron.predict(input_vec)
-    print(f"     {digit}     | {'Even' if prediction == 0 else 'Odd'}")
