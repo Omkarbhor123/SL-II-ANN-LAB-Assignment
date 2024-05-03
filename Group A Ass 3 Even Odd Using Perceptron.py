933ef94dcdf4e07fcaf5281fe1addfd8275d211f
Omkar Bhor <omkarbhor360@gmail.com>
2024-05-03 23:50:51 +0530

    Update Group A Ass 3 Even Odd Using Perceptron.py

--- Group A Ass 3 Even Odd Using Perceptron.py
+++ Group A Ass 3 Even Odd Using Perceptron.py
@@ -1,38 +1,39 @@
-# Define step function
-def step_function(x):
-    if x >= 0:
-        return 1
-    else:
-        return 0
+import numpy as np
+while True :
+  # Receive user input for j
+  j = int(input("Enter a Number (0-9): "))
 
-# Define training data
-training_data = [
-    {'input': [1, 1, 0, 0, 0, 0], 'label': 1},  # 0 (odd)
-    {'input': [1, 1, 0, 0, 0, 1], 'label': 0},  # 1 (even)
-    {'input': [1, 1, 0, 0, 1, 0], 'label': 1},  # 2 (odd)
-    {'input': [1, 1, 0, 1, 1, 1], 'label': 0},  # 3 (even)
-    {'input': [1, 1, 0, 1, 0, 0], 'label': 1},  # 4 (odd)
-    {'input': [1, 1, 0, 1, 0, 1], 'label': 0},  # 5 (even)
-    {'input': [1, 1, 0, 1, 1, 0], 'label': 1},  # 6 (odd)
-    {'input': [1, 1, 0, 1, 1, 1], 'label': 0},  # 7 (even)
-    {'input': [1, 1, 1, 0, 0, 0], 'label': 1},  # 8 (odd)
-    {'input': [1, 1, 1, 0, 0, 1], 'label': 0},  # 9 (even)
-]
+  # Define the step function
+  step_function = lambda x: 1 if x >= 0 else 0
 
-# Initialize weights
-weights = [0, 0, 0, 0, 0, 1]
+  # Define the training data
+  training_data = [
+      {'input': [1, 1, 0, 0, 0, 0], 'label': 1},
+      {'input': [1, 1, 0, 0, 0, 1], 'label': 0},
+      {'input': [1, 1, 0, 0, 1, 0], 'label': 1},
+      {'input': [1, 1, 0, 1, 1, 1], 'label': 0},
+      {'input': [1, 1, 0, 1, 0, 0], 'label': 1},
+      {'input': [1, 1, 0, 1, 0, 1], 'label': 0},
+      {'input': [1, 1, 0, 1, 1, 0], 'label': 1},
+      {'input': [1, 1, 0, 1, 1, 1], 'label': 0},
+      {'input': [1, 1, 1, 0, 0, 0], 'label': 1},
+      {'input': [1, 1, 1, 0, 0, 1], 'label': 0},
+  ]
 
-# Training the perceptron
-for data in training_data:
-    input_vector = data['input']
-    label = data['label']
-    output = step_function(sum(input_vector[i] * weights[i] for i in range(len(input_vector))))
-    error = label - output
-    weights = [weights[i] + input_vector[i] * error for i in range(len(weights))]
+  # Initialize weights
+  weights = np.array([0, 0, 0, 0, 0, 1])
 
-# Taking input from the user and predicting odd or even
-j = int(input("Enter a Number (0-9): "))
-input_vector = [int(x) for x in list('{0:06b}'.format(j))]
-output = "odd" if step_function(sum(input_vector[i] * weights[i] for i in range(len(input_vector)))) == 0 else "even"
-print(j, " is ", output)
+  # Training the perceptron
+  for data in training_data:
+      input_vector = np.array(data['input'])
+      label = data['label']
+      output = step_function(np.dot(input_vector, weights))
+      error = label - output
+      weights += input_vector * error
 
+  # Prepare input vector for user input
+  user_input = np.array([int(x) for x in list('{0:06b}'.format(j))])
+
+  # Predict if the user input is odd or even
+  output = "odd" if step_function(np.dot(user_input, weights)) == 0 else "even"
+  print(j, " is ", output)
